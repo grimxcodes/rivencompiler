b90beea30ee1909a1797f8c441d87f0c4e473071
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:14:53 +0530

    Update ir_instruction.hpp

--- include/ir/ir_instruction.hpp
+++ include/ir/ir_instruction.hpp
@@ -1,1 +1,45 @@
+#pragma once
 
+#include <string>
+
+namespace riven
+{
+
+enum class IRInstructionType
+{
+    LoadConst,
+
+    LoadVariable,
+    StoreVariable,
+
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+
+    CompareEqual,
+    CompareNotEqual,
+
+    CompareGreater,
+    CompareLess,
+
+    CompareGreaterEqual,
+    CompareLessEqual,
+
+    Jump,
+    JumpIfFalse,
+
+    Call,
+    Return
+};
+
+struct IRInstruction
+{
+    IRInstructionType type;
+
+    std::string operand1;
+    std::string operand2;
+    std::string result;
+};
+
+}
