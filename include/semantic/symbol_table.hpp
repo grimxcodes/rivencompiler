d5758bcab723999a283dca6d434b5ad2cd68ef65
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:04:14 +0530

    Update symbol_table.hpp

--- include/semantic/symbol_table.hpp
+++ include/semantic/symbol_table.hpp
@@ -1,1 +1,45 @@
+#pragma once
 
+#include <string>
+#include <unordered_map>
+
+namespace riven
+{
+
+enum class SymbolType
+{
+    Variable,
+    Constant,
+    Function,
+    Frame
+};
+
+struct Symbol
+{
+    std::string name;
+    SymbolType type;
+};
+
+class SymbolTable
+{
+public:
+    bool exists(
+        const std::string& name
+    ) const;
+
+    void define(
+        const Symbol& symbol
+    );
+
+    const Symbol* lookup(
+        const std::string& name
+    ) const;
+
+private:
+    std::unordered_map<
+        std::string,
+        Symbol
+    > m_symbols;
+};
+
+}
