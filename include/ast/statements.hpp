8753a3f011be9135f096fe195a4394e22d27a002
Ansh <grimxcodeskill@gmail.com>
2026-06-05 21:36:49 +0530

    Update statements.hpp

--- include/ast/statements.hpp
+++ include/ast/statements.hpp
@@ -5,62 +5,18 @@
 namespace riven
 {
 
-class StatementNode : public ASTNode
+class StatementNode
+    : public ASTNode
 {
 public:
     StatementNode(
         ASTNodeType type,
         SourceLocation location
     )
-        : ASTNode(type, location)
-    {
-    }
-};
-
-class ProgramNode final : public ASTNode
-{
-public:
-    std::vector<ASTNodePtr> declarations;
-
-    ProgramNode()
         : ASTNode(
-              ASTNodeType::Program,
-              SourceLocation()
-          )
-    {
-    }
-};
-
-class ImportNode final : public StatementNode
-{
-public:
-    std::string path;
-
-    ImportNode(
-        const std::string& importPath,
-        SourceLocation location
-    )
-        : StatementNode(
-              ASTNodeType::Import,
-              location
-          ),
-          path(importPath)
-    {
-    }
-};
-
-class CoreNode final : public StatementNode
-{
-public:
-    std::vector<ASTNodePtr> body;
-
-    CoreNode(
-        SourceLocation location
-    )
-        : StatementNode(
-              ASTNodeType::Core,
-              location
-          )
+            type,
+            location
+        )
     {
     }
 };
