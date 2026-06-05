832329d7f1b2f379c5b4fa7ddc4f821020d95422
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:15:17 +0530

    Update ir_module.hpp

--- include/ir/ir_module.hpp
+++ include/ir/ir_module.hpp
@@ -1,1 +1,27 @@
+#pragma once
 
+#include <vector>
+
+#include "ir_instruction.hpp"
+
+namespace riven
+{
+
+class IRModule
+{
+public:
+    void addInstruction(
+        const IRInstruction& instruction
+    );
+
+    const std::vector<
+        IRInstruction
+    >& instructions() const;
+
+private:
+    std::vector<
+        IRInstruction
+    > m_instructions;
+};
+
+}
