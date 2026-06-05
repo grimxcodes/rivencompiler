60435ae0da7cfb24f571038360a3b51151c16d04
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:07:00 +0530

    Update analyzer.hpp

--- include/semantic/analyzer.hpp
+++ include/semantic/analyzer.hpp
@@ -1,1 +1,35 @@
+#pragma once
 
+#include "../ast/statements.hpp"
+#include "../ast/declarations.hpp"
+
+#include "scope.hpp"
+
+namespace riven
+{
+
+class SemanticAnalyzer
+{
+public:
+    void analyze(
+        ProgramNode* program
+    );
+
+private:
+    Scope m_globalScope;
+
+private:
+    void analyzeNode(
+        ASTNode* node
+    );
+
+    void analyzeConstant(
+        ConstantNode* node
+    );
+
+    void analyzeFunction(
+        FunctionNode* node
+    );
+};
+
+}
