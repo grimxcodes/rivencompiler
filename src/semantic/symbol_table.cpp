5ee1d4e802e4fc669e4b3059e7f00bd8fa43b6ff
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:04:50 +0530

    Update symbol_table.cpp

--- src/semantic/symbol_table.cpp
+++ src/semantic/symbol_table.cpp
@@ -1,1 +1,38 @@
+#include "../../include/semantic/symbol_table.hpp"
 
+namespace riven
+{
+
+bool SymbolTable::exists(
+    const std::string& name
+) const
+{
+    return m_symbols.find(name)
+           != m_symbols.end();
+}
+
+void SymbolTable::define(
+    const Symbol& symbol
+)
+{
+    m_symbols[symbol.name] =
+        symbol;
+}
+
+const Symbol* SymbolTable::lookup(
+    const std::string& name
+) const
+{
+    auto iterator =
+        m_symbols.find(name);
+
+    if (iterator ==
+        m_symbols.end())
+    {
+        return nullptr;
+    }
+
+    return &iterator->second;
+}
+
+}
