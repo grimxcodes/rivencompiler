9ad7e7613749ce4e1815ae00132abd590d3a935e
Ansh <grimxcodeskill@gmail.com>
2026-06-05 18:48:51 +0530

    Update source_location.hpp

--- include/common/source_location.hpp
+++ include/common/source_location.hpp
@@ -1,1 +1,23 @@
-source
+#pragma once
+
+#include <cstddef>
+
+namespace riven
+{
+
+struct SourceLocation
+{
+    std::size_t line;
+    std::size_t column;
+
+    constexpr SourceLocation(
+        std::size_t lineNumber = 1,
+        std::size_t columnNumber = 1
+    )
+        : line(lineNumber),
+          column(columnNumber)
+    {
+    }
+};
+
+}
