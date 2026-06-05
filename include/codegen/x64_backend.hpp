1067e0ce7966f989e5d4795076656ed535e04ffa
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:19:11 +0530

    Update x64_backend.hpp

--- include/codegen/x64_backend.hpp
+++ include/codegen/x64_backend.hpp
@@ -1,1 +1,34 @@
+#pragma once
 
+#include "../ir/ir_module.hpp"
+
+#include <vector>
+#include <cstdint>
+
+namespace riven
+{
+
+class X64Backend
+{
+public:
+    std::vector<std::uint8_t>
+    generate(
+        const IRModule& module
+    );
+
+private:
+    void emit(
+        std::uint8_t byte
+    );
+
+    void emit32(
+        std::uint32_t value
+    );
+
+private:
+    std::vector<
+        std::uint8_t
+    > m_code;
+};
+
+}
