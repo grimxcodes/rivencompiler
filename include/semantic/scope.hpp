852f31f72447a250eae43502608743a751deb353
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:06:37 +0530

    Update scope.hpp

--- include/semantic/scope.hpp
+++ include/semantic/scope.hpp
@@ -1,1 +1,27 @@
+#pragma once
 
+#include <memory>
+
+#include "symbol_table.hpp"
+
+namespace riven
+{
+
+class Scope
+{
+public:
+    explicit Scope(
+        Scope* parent = nullptr
+    );
+
+    Scope* parent() const;
+
+    SymbolTable& symbols();
+
+private:
+    Scope* m_parent;
+
+    SymbolTable m_symbols;
+};
+
+}
