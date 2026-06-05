bfd60e517507bbd2a30def76175d594e99f859b3
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:00:45 +0530

    Update control_flow.hpp

--- include/ast/control_flow.hpp
+++ include/ast/control_flow.hpp
@@ -1,1 +1,64 @@
+#pragma once
 
+#include "statements.hpp"
+
+namespace riven
+{
+
+class IfNode final : public StatementNode
+{
+public:
+    ASTNodePtr condition;
+
+    std::vector<ASTNodePtr> body;
+
+    std::vector<ASTNodePtr> elseBody;
+
+    IfNode(
+        SourceLocation location
+    )
+        : StatementNode(
+              ASTNodeType::If,
+              location
+          )
+    {
+    }
+};
+
+class FlowNode final : public StatementNode
+{
+public:
+    ASTNodePtr count;
+
+    std::vector<ASTNodePtr> body;
+
+    FlowNode(
+        SourceLocation location
+    )
+        : StatementNode(
+              ASTNodeType::Flow,
+              location
+          )
+    {
+    }
+};
+
+class DuringNode final : public StatementNode
+{
+public:
+    ASTNodePtr condition;
+
+    std::vector<ASTNodePtr> body;
+
+    DuringNode(
+        SourceLocation location
+    )
+        : StatementNode(
+              ASTNodeType::During,
+              location
+          )
+    {
+    }
+};
+
+}
