ec5b6ae8a687f760757ad61fdda414980ad026e3
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:03:37 +0530

    Update parser.cpp

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -107,6 +107,31 @@ ASTNodePtr Parser::declaration()
         return parseImport();
     }
 
+    if (match(TokenType::Firm))
+    {
+        return parseFirm();
+    }
+
+    if (match(TokenType::Craft))
+    {
+        return parseFunction();
+    }
+
+    if (match(TokenType::If))
+    {
+        return parseIf();
+    }
+
+    if (match(TokenType::Flow))
+    {
+        return parseFlow();
+    }
+
+    if (match(TokenType::During))
+    {
+        return parseDuring();
+    }
+
     if (match(TokenType::Riven))
     {
         return parseCore();
@@ -123,7 +148,7 @@ ASTNodePtr Parser::parseImport()
     Token fileToken =
         consume(
             TokenType::String,
-            "Expected import file"
+            "Expected import path"
         );
 
     return std::make_unique<ImportNode>(
@@ -170,6 +195,249 @@ ASTNodePtr Parser::parseCore()
     );
 
     return node;
+}
+
+ASTNodePtr Parser::parseFirm()
+{
+    Token name =
+        consume(
+            TokenType::Identifier,
+            "Expected constant name"
+        );
+
+    consume(
+        TokenType::Assign,
+        "Expected '='"
+    );
+
+    auto value =
+        expression();
+
+    return std::make_unique<ConstantNode>(
+        name.lexeme,
+        std::move(value),
+        name.location
+    );
+}
+
+ASTNodePtr Parser::parseFunction()
+{
+    Token name =
+        consume(
+            TokenType::Identifier,
+            "Expected function name"
+        );
+
+    auto function =
+        std::make_unique<FunctionNode>(
+            name.lexeme,
+            name.location
+        );
+
+    consume(
+        TokenType::LeftParen,
+        "Expected '('"
+    );
+
+    while (!check(
+        TokenType::RightParen))
+    {
+        Token parameter =
+            consume(
+                TokenType::Identifier,
+                "Expected parameter"
+            );
+
+        function->parameters.push_back(
+            parameter.lexeme
+        );
+
+        if (!match(
+            TokenType::Comma))
+        {
+            break;
+        }
+    }
+
+    consume(
+        TokenType::RightParen,
+        "Expected ')'"
+    );
+
+    consume(
+        TokenType::LeftBrace,
+        "Expected '{'"
+    );
+
+    while (!check(
+        TokenType::RightBrace))
+    {
+        if (isAtEnd())
+        {
+            throw ParserError(
+                "Expected '}'",
+                peek().location
+            );
+        }
+
+        advance();
+    }
+
+    consume(
+        TokenType::RightBrace,
+        "Expected '}'"
+    );
+
+    return function;
+}
+
+ASTNodePtr Parser::parseIf()
+{
+    auto node =
+        std::make_unique<IfNode>(
+            previous().location
+        );
+
+    node->condition =
+        expression();
+
+    consume(
+        TokenType::LeftBrace,
+        "Expected '{'"
+    );
+
+    while (!check(
+        TokenType::RightBrace))
+    {
+        if (isAtEnd())
+        {
+            throw ParserError(
+                "Expected '}'",
+                peek().location
+            );
+        }
+
+        advance();
+    }
+
+    consume(
+        TokenType::RightBrace,
+        "Expected '}'"
+    );
+
+    return node;
+}
+
+ASTNodePtr Parser::parseFlow()
+{
+    auto node =
+        std::make_unique<FlowNode>(
+            previous().location
+        );
+
+    node->count =
+        expression();
+
+    consume(
+        TokenType::LeftBrace,
+        "Expected '{'"
+    );
+
+    while (!check(
+        TokenType::RightBrace))
+    {
+        if (isAtEnd())
+        {
+            throw ParserError(
+                "Expected '}'"
+            );
+        }
+
+        advance();
+    }
+
+    consume(
+        TokenType::RightBrace,
+        "Expected '}'"
+    );
+
+    return node;
+}
+
+ASTNodePtr Parser::parseDuring()
+{
+    auto node =
+        std::make_unique<DuringNode>(
+            previous().location
+        );
+
+    node->condition =
+        expression();
+
+    consume(
+        TokenType::LeftBrace,
+        "Expected '{'"
+    );
+
+    while (!check(
+        TokenType::RightBrace))
+    {
+        if (isAtEnd())
+        {
+            throw ParserError(
+                "Expected '}'"
+            );
+        }
+
+        advance();
+    }
+
+    consume(
+        TokenType::RightBrace,
+        "Expected '}'"
+    );
+
+    return node;
+}
+
+ASTNodePtr Parser::expression()
+{
+    return primary();
+}
+
+ASTNodePtr Parser::primary()
+{
+    if (match(TokenType::Number))
+    {
+        return std::make_unique<
+            NumberLiteralNode>(
+            previous().lexeme,
+            previous().location
+        );
+    }
+
+    if (match(TokenType::String))
+    {
+        return std::make_unique<
+            StringLiteralNode>(
+            previous().lexeme,
+            previous().location
+        );
+    }
+
+    if (match(TokenType::Identifier))
+    {
+        return std::make_unique<
+            IdentifierNode>(
+            previous().lexeme,
+            previous().location
+        );
+    }
+
+    throw ParserError(
+        "Invalid expression",
+        peek().location
+    );
 }
 
 }
