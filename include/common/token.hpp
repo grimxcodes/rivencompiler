bddfec4dda3d37850fccde83f76e95bc92b6bbab
Ansh <grimxcodeskill@gmail.com>
2026-06-05 18:49:52 +0530

    Update token.hpp

--- include/common/token.hpp
+++ include/common/token.hpp
@@ -1,1 +1,29 @@
+#pragma once
 
+#include <string>
+
+#include "token_type.hpp"
+#include "source_location.hpp"
+
+namespace riven
+{
+
+struct Token
+{
+    TokenType type;
+    std::string lexeme;
+    SourceLocation location;
+
+    Token(
+        TokenType tokenType,
+        std::string text,
+        SourceLocation sourceLocation
+    )
+        : type(tokenType),
+          lexeme(std::move(text)),
+          location(sourceLocation)
+    {
+    }
+};
+
+}
