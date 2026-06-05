2fd7f8c223ff97d672a03783da05f17dfcd7fc24
Ansh <grimxcodeskill@gmail.com>
2026-06-05 21:31:27 +0530

    Update lexer.hpp

--- include/lexer/lexer.hpp
+++ include/lexer/lexer.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "../common/token.hpp"
 
@@ -11,31 +12,41 @@ namespace riven
 class Lexer
 {
 public:
-    explicit Lexer(const std::string& source);
+    explicit Lexer(
+        const std::string& source
+    );
 
     std::vector<Token> tokenize();
 
 private:
-    std::string m_source;
+    const std::string m_source;
 
     std::size_t m_position = 0;
+
     std::size_t m_line = 1;
+
     std::size_t m_column = 1;
 
 private:
     bool isAtEnd() const;
 
     char peek() const;
+
     char peekNext() const;
 
     char advance();
 
+private:
     void skipWhitespace();
 
-    Token identifier();
-    Token number();
+private:
     Token string();
 
+    Token number();
+
+    Token identifier();
+
+private:
     Token makeToken(
         TokenType type,
         const std::string& lexeme,
