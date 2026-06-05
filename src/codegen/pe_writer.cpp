47fb2851b1d69a38af96569bd5d9dcb6f6174a30
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:20:41 +0530

    Update pe_writer.cpp

--- src/codegen/pe_writer.cpp
+++ src/codegen/pe_writer.cpp
@@ -1,1 +1,37 @@
+#include "../../include/codegen/pe_writer.hpp"
 
+#include <fstream>
+
+namespace riven
+{
+
+bool PEWriter::write(
+    const std::string& outputFile,
+    const std::vector<
+        std::uint8_t
+    >& machineCode
+)
+{
+    std::ofstream file(
+        outputFile,
+        std::ios::binary
+    );
+
+    if (!file)
+    {
+        return false;
+    }
+
+    file.write(
+        reinterpret_cast<
+            const char*
+        >(machineCode.data()),
+        static_cast<
+            std::streamsize
+        >(machineCode.size())
+    );
+
+    return true;
+}
+
+}
