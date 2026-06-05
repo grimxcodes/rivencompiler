501afb31ee3e64654d9b1dd60e6d632295a32e2b
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:15:48 +0530

    Update ir_builder.hpp

--- include/ir/ir_builder.hpp
+++ include/ir/ir_builder.hpp
@@ -1,1 +1,44 @@
+#pragma once
 
+#include "../ast/statements.hpp"
+#include "../ast/declarations.hpp"
+#include "../ast/expressions.hpp"
+
+#include "ir_module.hpp"
+
+namespace riven
+{
+
+class IRBuilder
+{
+public:
+    IRModule build(
+        ProgramNode* program
+    );
+
+private:
+    IRModule m_module;
+
+    int m_tempCounter = 0;
+
+private:
+    void generateNode(
+        ASTNode* node
+    );
+
+    void generateConstant(
+        ConstantNode* node
+    );
+
+    void generateFunction(
+        FunctionNode* node
+    );
+
+    std::string generateExpression(
+        ASTNode* node
+    );
+
+    std::string createTemp();
+};
+
+}
