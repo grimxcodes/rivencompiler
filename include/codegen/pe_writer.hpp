7008a0066693a18e9b4ecb5a69416484ea0953a9
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:19:37 +0530

    Update pe_writer.hpp

--- include/codegen/pe_writer.hpp
+++ include/codegen/pe_writer.hpp
@@ -1,1 +1,21 @@
+#pragma once
 
+#include <string>
+#include <vector>
+#include <cstdint>
+
+namespace riven
+{
+
+class PEWriter
+{
+public:
+    bool write(
+        const std::string& outputFile,
+        const std::vector<
+            std::uint8_t
+        >& machineCode
+    );
+};
+
+}
