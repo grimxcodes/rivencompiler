0c03ee65ebf97ba2a5564476205b9de969c6170f
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:05:28 +0530

    Update scope.cpp

--- src/semantic/scope.cpp
+++ src/semantic/scope.cpp
@@ -1,1 +1,23 @@
+#include "../../include/semantic/scope.hpp"
 
+namespace riven
+{
+
+Scope::Scope(
+    Scope* parent
+)
+    : m_parent(parent)
+{
+}
+
+Scope* Scope::parent() const
+{
+    return m_parent;
+}
+
+SymbolTable& Scope::symbols()
+{
+    return m_symbols;
+}
+
+}
