c816e1d5b6df34d6287737694e0fc62e70ff489d
Ansh <grimxcodeskill@gmail.com>
2026-06-05 19:16:56 +0530

    Update ir_module.cpp

--- src/ir/ir_module.cpp
+++ src/ir/ir_module.cpp
@@ -1,1 +1,22 @@
+#include "../../include/ir/ir_module.hpp"
 
+namespace riven
+{
+
+void IRModule::addInstruction(
+    const IRInstruction& instruction
+)
+{
+    m_instructions.push_back(
+        instruction
+    );
+}
+
+const std::vector<
+    IRInstruction
+>& IRModule::instructions() const
+{
+    return m_instructions;
+}
+
+}
