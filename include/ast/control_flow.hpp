f7c747074cee730cf7b624fd26c5b8e8ac501d87
Ansh <grimxcodeskill@gmail.com>
2026-06-06 08:35:59 +0530

    Update control_flow.hpp

--- include/ast/control_flow.hpp
+++ include/ast/control_flow.hpp
@@ -5,58 +5,79 @@
 namespace riven
 {
 
-class IfNode final : public StatementNode
+class IfNode final
+    : public StatementNode
 {
 public:
     ASTNodePtr condition;
 
-    std::vector<ASTNodePtr> body;
+    std::vector<
+        ASTNodePtr
+    > body;
 
-    std::vector<ASTNodePtr> elseBody;
+    std::vector<
+        ASTNodePtr
+    > altIfConditions;
+
+    std::vector<
+        std::vector<
+            ASTNodePtr
+        >
+    > altIfBodies;
+
+    std::vector<
+        ASTNodePtr
+    > elseBody;
 
     IfNode(
         SourceLocation location
     )
         : StatementNode(
-              ASTNodeType::If,
-              location
-          )
+            ASTNodeType::If,
+            location
+        )
     {
     }
 };
 
-class FlowNode final : public StatementNode
+class FlowNode final
+    : public StatementNode
 {
 public:
     ASTNodePtr count;
 
-    std::vector<ASTNodePtr> body;
+    std::vector<
+        ASTNodePtr
+    > body;
 
     FlowNode(
         SourceLocation location
     )
         : StatementNode(
-              ASTNodeType::Flow,
-              location
-          )
+            ASTNodeType::Flow,
+            location
+        )
     {
     }
 };
 
-class DuringNode final : public StatementNode
+class DuringNode final
+    : public StatementNode
 {
 public:
     ASTNodePtr condition;
 
-    std::vector<ASTNodePtr> body;
+    std::vector<
+        ASTNodePtr
+    > body;
 
     DuringNode(
         SourceLocation location
     )
         : StatementNode(
-              ASTNodeType::During,
-              location
-          )
+            ASTNodeType::During,
+            location
+        )
     {
     }
 };
